17aedb819bffe9b8c2af69e40caa40850f2bd466
VorobevDenis <den001v@gmail.com>
2022-03-01 18:34:05 +0700

    Выполнено задание второго дня

--- store/settings.py
+++ store/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products'
+
+    'products',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -123,7 +125,13 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Users
+AUTH_USER_MODEL = 'users.User'
