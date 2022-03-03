0f36d75653a53279d1fb057f3ce02f4209eabe60
VorobevDenis <den001v@gmail.com>
2022-03-04 00:22:34 +0700

    Выполнено задание четвертого дня

--- products/models.py
+++ products/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 
-# Create your models here.
-
+# модели = таблицы в БД
 class ProductCategory(models.Model):
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField(blank=True, null=True)
